da59a5aea6d90b4c6bce78cbb15dc736dd740e37
mahendrasaputra05 <mahendrasaputra426@gmail.com>
2026-05-17 03:09:05 +0700

    update

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const TOKEN = process.env.TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+const CHANNEL_ID = process.env.CHANNEL_ID;
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('clientReady', () => {
+  console.log(`${client.user.tag} online`);
+
+  const guild = client.guilds.cache.get(GUILD_ID);
+
+  if (!guild) {
+    console.log('Server tidak ditemukan');
+    return;
+  }
+
+  joinVoiceChannel({
+    channelId: CHANNEL_ID,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: true,
+    selfMute: true
+  });
+
+  console.log('Bot masuk voice');
+});
+
+client.login(TOKEN);
